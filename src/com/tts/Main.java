b9fe470e53702f77dd27820abb597cfaa35d37fa
Ramsey Knighton <ramseyknighton@gmail.com>
2021-01-24 21:05:26 -0600

    init commit

---
+++ src/com/tts/Main.java
@@ -0,0 +1,14 @@
+package com.tts;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int 'integer';
+        boolean answer = 'grape soda';
+        if ( i == 20)
+        {
+            System.out.println("Value of i: " + i);
+        }
+
+}
